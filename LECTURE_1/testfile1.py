c9feca81f335cf0ff5e1a94446da0f49c0f652c4
guardianfrancie191-rgb <guardianfrancie191@gmail.com>
2025-10-25 16:33:31 +0800

    Week 4 Activities

---
+++ LECTURE_1/testfile1.py
@@ -0,0 +1,6 @@
+print ('hello world')
+print('Francie said: "Hello World"')
+input("Enter your name:")
+input("Enter your age:")
+
+
---
+++ LECTURE_1/testfile2.py
@@ -0,0 +1,19 @@
+myGreetings = ("Hellow World")
+Age = 16
+
+print(myGreetings)
+print(Age)
+combineGreetAndAge1 = "Hellow World" + str(16)
+combineGreetAndAge2 = myGreetings + str(Age)
+
+print(combineGreetAndAge1)
+print(combineGreetAndAge2)
+
+a = input("Enter first number : ")
+b = input("Enter second number : ")
+c = input("Enter third number : ")
+
+mySum = int(a) + int(b) + int(c)
+sumInt = int(a) + int(b) + int(c)
+print(mySum)
+print(sumInt)
